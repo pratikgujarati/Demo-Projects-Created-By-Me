f14216345c160cedfe28f18bd8687397bf02a1dd
YogiBhimani <bhimani.yogi@gmail.com>
2015-06-13 10:58:35 +0530

    Initial Commit

---
+++ TwitterLoginDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterLoginDemo
+//
+//  Created by Infusion on 6/13/15.
+//  Copyright (c) 2015 Infusion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterLoginDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TwitterLoginDemo
+//
+//  Created by Infusion on 6/13/15.
+//  Copyright (c) 2015 Infusion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
